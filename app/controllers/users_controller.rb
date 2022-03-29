6b9f22fa9a8415de28f4b725a2b19dbd4e89930d
ayrton <ayrton.parkinson1@gmail.com>
2022-03-29 07:50:12 -0400

    Fix issue with rendering errors in #create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
